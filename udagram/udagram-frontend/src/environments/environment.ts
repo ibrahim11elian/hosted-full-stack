1169bb920fd7b0c12d89a7d5c57ba32c1ba8372d
Ibrahim Ahmed Abdelnaby <58626527+ibrahim11elian@users.noreply.github.com>
2022-09-16 05:51:38 +0200

    fix

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: "Udagram",
-  apiHost: "http://localhost:3000/api/v0",
+  apiHost:
+    "http://udagram-api-dev.eba-6c2tynvf.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
