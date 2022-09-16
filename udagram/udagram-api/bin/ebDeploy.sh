e2c38f890ed4b472fc002bc55b398fe9cf032a44
Ibrahim Ahmed Abdelnaby <58626527+ibrahim11elian@users.noreply.github.com>
2022-09-16 14:15:41 +0200

    fix

--- udagram/udagram-api/bin/ebDeploy.sh
+++ udagram/udagram-api/bin/ebDeploy.sh
@@ -4,4 +4,7 @@ eb init udagram-api --platform node.js --region us-east-1
 eb create --sample udagram-api-dev
 eb use udagram-api-dev
 npm run setenv
+npm run build
+echo hi we are at $(pwd)
+ls -la
 eb deploy udagram-api-dev
